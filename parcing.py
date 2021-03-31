e63c321b321133b1d780028f8f75ef2123728e70
Evgeniy Khlystov <hlystovea@gmail.com>
2021-03-31 22:41:14 +0700

    added inflow and outflow parcing

--- parcing.py
+++ parcing.py
@@ -14,13 +14,11 @@ def parcing_bwu():
     page = requests.get(url)
 
     soup = BeautifulSoup(page.text, 'html.parser')
-    parcings_levels = soup.find_all(string=re.compile('верхний бьеф'))
-    parcings_flow1 = soup.find_all(string=re.compile('приток [0-9]'))
-    parcings_flow2 = soup.find_all(string=re.compile('приток общий'))
-    #for z in range(len(parcings_flow1)):
-        #print(parcings_flow1[z])
-    #for z in range(len(parcings_flow2)):
-        #print(parcings_flow2[z])
+    data = soup.find_all(string=re.compile('верхний бьеф'))
+    inflow_sayan = soup.find_all(string=re.compile('приток [0-9]'))
+    inflow_kras_bratsk = soup.find_all(string=re.compile('приток общий'))
+    inflow_kras = inflow_kras_bratsk[0::2]
+    inflow_bratsk = inflow_kras_bratsk[1::2]
     parcings_dates = soup.find_all(
         string=re.compile('[0-3][0-9][.][01][0-9][.][2][0][2-9][0-9].[г.]')
         )
@@ -32,28 +30,46 @@ def parcing_bwu():
 
     for i in range(0, 5):  # Итерации по 5 строкам парсинга
         date_str = parcings_dates[i].split()[0]
-        date = datetime.strptime(date_str, '%d.%m.%Y').timestamp()
-        values = []
-        val = []
-        values.append(date)
-        val.append(date)
+        try:
+            date = datetime.strptime(date_str, '%d.%m.%Y').timestamp()
+        except ValueError as error:
+            print(f'Произошла ошибка: {error}')
+            break
+        levels = []
+        levels.append(date)
+        outflows = []
+        outflows.append(date)
+        inflows = []
+        inflows = [
+            date,
+            int(inflow_sayan[i].split()[2]),
+            int(inflow_kras[i].split()[3]),
+            int(inflow_bratsk[i].split()[3])
+        ]
         for n in range(0, 6):  # Подготовка строки для записи в бд
-            level = (parcings_levels[i*6+n].split()[3])
-            avg_flow = (parcings_levels[i*6+n].split()[13])
+            level = data[i*6+n].split()[3]
             level = float(level.replace(',', '.'))
-            values.append(level)
-            val.append(avg_flow)
-        #print(val)
-        #print(values)
+            levels.append(level)
+            outflow = int(data[i*6+n].split()[13])
+            outflows.append(outflow)
         try:
             cursor.execute(
                 'INSERT INTO uvb VALUES(?, ?, ?, ?, ?, ?, ?);',
-                tuple(values)
+                tuple(levels),
                 )
-            conn.commit()
-            print(f'Добавлены данные за {date_str}')
-        except sqlite3.IntegrityError:
+            cursor.execute(
+                'INSERT INTO outflow VALUES(?, ?, ?, ?, ?, ?, ?);',
+                tuple(outflows),
+                )
+            cursor.execute(
+                'INSERT INTO inflow VALUES(?, ?, ?, ?);',
+                tuple(inflows),
+                )
+        except sqlite3.IntegrityError as error:
             continue
+        else:
+            print(f'Добавлены данные за {date_str}')
+            conn.commit()
     cursor.close()
     conn.close()
 
