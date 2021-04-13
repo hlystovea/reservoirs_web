1f78c11cbbaa4048f0534309358652b5d23c34e3
Evgeniy Khlystov <hlystovea@gmail.com>
2021-04-13 21:10:11 +0700

    PEP8, separation of functions

--- parcing.py
+++ parcing.py
@@ -7,78 +7,92 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parcing_bwu():
-    # Парсинг сайта Енисейского БВУ
-    url = 'http://enbvu.ru/i03_deyatelnost/i03.07_vdho.php'
-
-    page = requests.get(url)
-
-    soup = BeautifulSoup(page.text, 'html.parser')
-    data = soup.find_all(string=re.compile('верхний бьеф'))
-    inflow_sayan = soup.find_all(string=re.compile('приток [0-9]'))
-    inflow_kras_bratsk = soup.find_all(string=re.compile('приток общий'))
-    inflow_kras = inflow_kras_bratsk[0::2]
-    inflow_bratsk = inflow_kras_bratsk[1::2]
-    parcings_dates = soup.find_all(
-        string=re.compile('[0-3]?[0-9][.][01][0-9][.][2][0][2-9][0-9].[г.]')
-        )
-    # Добавление данных об уровнях водохранилищ в базу данных
-    # Подключение к базе данных
+def db_saver(records: dict):
     conn = sqlite3.connect('levels.sqlite')
     cursor = conn.cursor()
-
-    for i in range(0, 5):  # Итерации по 5 строкам парсинга
-        date_str = parcings_dates[i].split()[0]
-        try:
-            date = datetime.strptime(date_str, '%d.%m.%Y').timestamp()
-        except ValueError as error:
-            print(f'Произошла ошибка: {error}')
-            break
-        levels = []
-        levels.append(date)
-        outflows = []
-        outflows.append(date)
-        inflows = [
-            date,
-            int(inflow_sayan[i].split()[2]),
-            int(inflow_kras[i].split()[3]),
-            int(inflow_bratsk[i].split()[3])
-        ]
-        for n in range(0, 6):  # Подготовка строки для записи в бд
-            level = data[i*6+n].split()[3]
-            level = float(level.replace(',', '.'))
-            levels.append(level)
-            outflow = int(data[i*6+n].split()[13])
-            outflows.append(outflow)
+    for i in range(len(records['levels'])):
         try:
             sql_level = '''\
                 INSERT INTO uvb
                 VALUES(?, ?, ?, ?, ?, ?, ?)
                 '''
-            cursor.execute(sql_level, tuple(levels))
+            cursor.execute(sql_level, records['levels'][i])
             sql_outflow = '''\
                 INSERT INTO outflow
                 VALUES(?, ?, ?, ?, ?, ?, ?)
                 '''
-            cursor.execute(sql_outflow, tuple(outflows))
+            cursor.execute(sql_outflow, records['outflows'][i])
             sql_inflow = '''\
                 INSERT INTO inflow (date, sayany, kras, bratsk)
                 VALUES (?, ?, ?, ?)
                 '''
-            cursor.execute(sql_inflow, tuple(inflows))
+            cursor.execute(sql_inflow, records['inflows'][i])
         except sqlite3.IntegrityError:
             continue
         else:
-            print(f'Добавлены данные за {date_str}')
+            print(f'Добавлены данные за {records[0][0]}')
             conn.commit()
     cursor.close()
     conn.close()
 
 
+def parcing(url: str) -> dict:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    data = soup.find_all(string=re.compile('верхний бьеф'))
+
+    inflow_sayan = soup.find_all(string=re.compile('приток [0-9]'))
+    inflow_kras_bratsk = soup.find_all(string=re.compile('приток общий'))
+    inflow_kras = inflow_kras_bratsk[0::2]
+    inflow_bratsk = inflow_kras_bratsk[1::2]
+
+    date_re = '[0-3]?[0-9][.][01][0-9][.][2][0][2-9][0-9].[г.]'
+    dates = soup.find_all(string=re.compile(date_re))
+
+    levels = []
+    outflows = []
+    inflows = []
+
+    for day in range(len(dates)):
+        try:
+            date = dates[day].split()[0]
+            date = datetime.strptime(date, '%d.%m.%Y').timestamp()
+        except ValueError as error:
+            print(f'Произошла ошибка: {error}')
+            break
+
+        lvls = [level.split()[3] for level in data[day*6:day*6+6]]
+        lvls = [float(level.replace(',', '.')) for level in lvls]
+        lvls.insert(0, date)
+
+        outflws = [outflow.split()[13] for outflow in data[day*6:day*6+6]]
+        outflws = [int(outflow) for outflow in outflws]
+        outflws.insert(0, date)
+
+        inflws = [
+            date,
+            int(inflow_sayan[day].split()[2]),
+            int(inflow_kras[day].split()[3]),
+            int(inflow_bratsk[day].split()[3])
+        ]
+        levels.append(tuple(lvls))
+        outflows.append(tuple(outflws))
+        inflows.append(tuple(inflws))
+
+    records = {
+        'levels': tuple(levels),
+        'outflows': tuple(outflows),
+        'inflows': tuple(inflows),
+    }
+    return records
+
+
 def timer():
     while True:
-        parcing_bwu()
-        time.sleep(10800)
+        url = 'http://enbvu.ru/i03_deyatelnost/i03.07_vdho.php'
+        db_saver(parcing(url))
+        time.sleep(3000)
 
 
 if __name__ == '__main__':
