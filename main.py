52a5806d7160a750a68ef2306cf22a4fd6c4758e
Evgeniy Khlystov <hlystovea@gmail.com>
2021-04-18 21:30:07 +0700

    bot v.0.9.0

--- main.py
+++ main.py
@@ -7,53 +7,75 @@ from logging.handlers import RotatingFileHandler
 
 import requests
 import telebot
-from telebot.types import (ForceReply, InlineKeyboardButton,
-                           InlineKeyboardMarkup, ReplyKeyboardMarkup)
+from telebot.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
 from utils import Plotter, Reservoir, TimePeriod, res_param
+
 
 TOKEN = os.environ.get('BOT_BWU')
 
 bot = telebot.TeleBot(TOKEN)
 
-rotate_file_handler = RotatingFileHandler(
-    'log.log',
-    maxBytes=5000000,
-    backupCount=2,
-)
-console_out_hundler = logging.StreamHandler(
-)
+logger = telebot.logger
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s %(funcName)s - %(message)s')
+rf_handler = RotatingFileHandler('bot.log', maxBytes=5000000, backupCount=2)
+rf_handler.setFormatter(formatter)
+logger.addHandler(rf_handler)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(funcName)s: %(message)s',
-    handlers=[console_out_hundler, rotate_file_handler],
-)
+
+class Graph():
+    def __init__(self, reservoir) -> None:
+        self.reservoir = reservoir
+        self.param = None
+        self.period = None
+
+
+graph_buffer = {}
 
 
 start_keyboard = ReplyKeyboardMarkup(True, selective=True)
-start_keyboard.row('Показать список')
+start_keyboard.row('Показать список водохранилищ')
+
 
 res_keyboard = InlineKeyboardMarkup()
-for r in sorted(res_param):
+for res in sorted(res_param):
     button = InlineKeyboardButton(
-        res_param[r][0],
-        callback_data=f'reservoir {r}',
+        res_param[res][0],
+        callback_data=res,
     )
     res_keyboard.add(button)
 
-time_keyboard = InlineKeyboardMarkup()
-week = InlineKeyboardButton(text='Месяц', callback_data='fixed 30')
-month = InlineKeyboardButton(text='Пол года', callback_data='fixed 183')
-month3 = InlineKeyboardButton(text='Год', callback_data='fixed 365')
-year = InlineKeyboardButton(text='Всё время', callback_data='fixed 10000')
-manually = InlineKeyboardButton(text='Ввести даты вручную', callback_data='manually') # noqa
-info = InlineKeyboardButton(text='Вывести информацию о ГЭС', callback_data='get_info') # noqa
-inflow = InlineKeyboardButton(text='Построить график притока', callback_data='inflow') # noqa
-outflow = InlineKeyboardButton(text='Построить график сброса', callback_data='outflow') # noqa
-time_keyboard.row(week, month, month3, year).add(manually).add(info).add(inflow).add(outflow) # noqa
 
-choice = {}
+command_buttons = {
+    'levels': 'Построить график УВБ',
+    'inflow': 'Построить график притока',
+    'outflow': 'Построить график сброса',
+    'info': 'Информация о водохранилище',
+    'back_to_reservoirs': 'Назад',
+}
+
+command_keyboard = InlineKeyboardMarkup()
+for command in command_buttons:
+    button = InlineKeyboardButton(
+        command_buttons[command],
+        callback_data=command,
+    )
+    command_keyboard.add(button)
+
+
+time_keyboard = InlineKeyboardMarkup()
+week = InlineKeyboardButton('Неделя', callback_data='fixed&7')
+month = InlineKeyboardButton('Месяц', callback_data='fixed&30')
+six_month = InlineKeyboardButton('Пол года', callback_data='fixed&183')
+year = InlineKeyboardButton('Год', callback_data='fixed&365')
+all_time = InlineKeyboardButton('За всё время', callback_data='fixed&10000')
+manual = InlineKeyboardButton('Ввести даты вручную', callback_data='manually')
+back = InlineKeyboardButton('Назад', callback_data='back_to_commands')
+time_keyboard.row(week, month, six_month, year)
+time_keyboard.add(all_time).add(manual).add(back)
 
 
 # Start message
@@ -61,8 +83,9 @@ choice = {}
 def start(message):
     text = ('Привет! Я бот, который умеет по данным [Енисейского БВУ]'
             '(http://enbvu.ru/i03_deyatelnost/i03.07_vdho.php) строить '
-            'графики уровней водохранилищ Ангаро-Енисейского каскада ГЭС. '
-            'Чтобы начать, нажмите *"Показать список"*')
+            'графики УВБ, сброса и притока воды в водохранилища '
+            'Ангаро-Енисейского каскада ГЭС. '
+            'Чтобы начать, нажмите *"Показать список водохранилищ"*.')
     bot.send_message(
         message.chat.id,
         text,
@@ -72,184 +95,143 @@ def start(message):
     )
 
 
-@bot.message_handler(commands=['stat'])
-def statistic(message):
-    period = TimePeriod('01.01.1990')
-    pic = Plotter.plot_statistics(period)
-    bot.delete_message(message.chat.id, message.message_id)
-    bot.send_photo(message.chat.id, pic)
-
-
-# Choice of period and plotting
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-    if 'info' in call.data:
-        bot.answer_callback_query(call.id)
-        res = Reservoir(choice['res'])
-        res.add_statistic()
+    logger.info(call.data)
+    bot.answer_callback_query(call.id, 'Секунду..')
+    if call.data in res_param:
+        reservoir = Reservoir(call.data)
+        graph = Graph(reservoir)
+        graph_buffer[call.message.chat.id] = graph
+        text = 'Выберите команду:'
+        bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=command_keyboard,
+        )
+    elif call.data == 'info':
         text = 'Раздел находится в разработке.'
         bot.edit_message_text(
             text,
             call.message.chat.id,
             call.message.message_id,
         )
-        choice.clear()
-    elif 'reservoir' in call.data:
-        res = call.data.split()[1]
-        choice['res'] = res
-        bot.answer_callback_query(call.id)
-        text1 = f'{res_param[res][0]}:'
-        text2 = 'Выберите период, за который необходимо построить график:'
-        bot.edit_message_text(
-            text1,
+    elif call.data == 'manually':
+        text = ('Введите начало и конец периода '
+                'через пробел в формате dd.mm.yyyy')
+        msg = bot.edit_message_text(
+            text,
             call.message.chat.id,
             call.message.message_id,
         )
-        bot.send_message(
-            call.message.chat.id,
-            text2,
-            reply_markup=time_keyboard
-        )
-    elif 'manually' in call.data:
-        text = 'Введите начало и конец периода через пробел в формате dd.mm.yyyy'
-        bot.delete_message(call.message.chat.id, call.message.message_id)
-        bot.send_message(
-            call.message.chat.id,
-            text,
-            reply_markup=ForceReply())
+        bot.register_next_step_handler(msg, manually_input_step)
     elif 'fixed' in call.data:
-        bot.answer_callback_query(call.id, text='Уже рисую..')
-        days = int(call.data.split()[1])
+        graph = graph_buffer[call.message.chat.id]
+        days = int(call.data.split('&')[1])
         date2 = date.today()
         date1 = date2 - timedelta(days)
-        period = TimePeriod(date1.isoformat(), date2.isoformat(), date_format='%Y-%m-%d')
-        try:
-            res = Reservoir(choice['res'])
-            res.add_statistic()
-            pic, answer, is_success = Plotter.plot_levels(res, period)
-            bot.edit_message_text(
-                answer,
-                call.message.chat.id,
-                call.message.message_id,
-            )
-            if is_success:
-                bot.send_photo(call.message.chat.id, pic)
-        except KeyError:
-            text = (
-                'Что-то пошло не так. '
-                'Попробуйте выбрать водохранилище ещё раз.')
-            bot.delete_message(call.message.chat.id, call.message.message_id)
+        period = TimePeriod(
+            date1.isoformat(),
+            date2.isoformat(),
+            date_format='%Y-%m-%d',
+        )
+        graph.period = period
+        pic, text, is_success = plotting_graph(graph)
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        if is_success:
+            bot.send_photo(call.message.chat.id, pic, text)
+        else:
             bot.send_message(call.message.chat.id, text)
-        choice.clear()
-    elif 'inflow' in call.data:
-        bot.answer_callback_query(call.id, text='Уже рисую..')
-        period = TimePeriod('01.01.1990')
-        try:
-            res = Reservoir(choice['res'])
-            res.add_statistic()
-            pic, answer, is_success = Plotter.plot_inflow(res, period)
-            bot.edit_message_text(
-                answer,
-                call.message.chat.id,
-                call.message.message_id,
-            )
-            if is_success:
-                bot.send_photo(call.message.chat.id, pic)
-        except KeyError:
-            text = (
-                'Что-то пошло не так. '
-                'Попробуйте выбрать водохранилище ещё раз.')
-            bot.delete_message(call.message.chat.id, call.message.message_id)
-            bot.send_message(call.message.chat.id, text)
-        choice.clear()
-    elif 'outflow' in call.data:
-        bot.answer_callback_query(call.id, text='Уже рисую..')
-        period = TimePeriod('01.01.1990')
-        try:
-            res = Reservoir(choice['res'])
-            res.add_statistic()
-            pic, answer, is_success = Plotter.plot_outflow(res, period)
-            bot.edit_message_text(
-                answer,
-                call.message.chat.id,
-                call.message.message_id,
-            )
-            if is_success:
-                bot.send_photo(call.message.chat.id, pic)
-        except KeyError:
-            text = (
-                'Что-то пошло не так. '
-                'Попробуйте выбрать водохранилище ещё раз.')
-            bot.delete_message(call.message.chat.id, call.message.message_id)
-            bot.send_message(call.message.chat.id, text)
-        choice.clear()
+    elif call.data in ('levels', 'inflow', 'outflow'):
+        graph = graph_buffer[call.message.chat.id]
+        graph.param = call.data
+        text = 'Выберите период, за который необходимо построить график:'
+        bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=time_keyboard,
+        )
+    elif call.data == 'back_to_reservoirs':
+        text = 'Выберите водохранилище:'
+        bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=res_keyboard,
+        )
+    elif call.data == 'back_to_commands':
+        text = 'Выберите водохранилище:'
+        bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=command_keyboard,
+        )
     else:
         text = 'Упс.. похоже я не знаю ответ.'
         bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.send_message(call.message.chat.id, text)
 
 
-# Inputing period manually
-@bot.message_handler(regexp='[0-3][0-9][.][01][0-9][.][12][09][0-9][0-9]')
-def manually_plot(message):
-    if 'res' in choice:
-        try:
-            dates = re.findall(
-                r'[0-3][0-9][.][01][0-9][.][12][09][0-9][0-9]',
-                message.text
-            )
-            date1 = dates[0]
-            date2 = None
-            if len(dates) > 1:
-                date2 = dates[-1]
-            period = TimePeriod(date1, date2, date_format='%d.%m.%Y')
-            res = Reservoir(choice['res'])
-            res.add_statistic()
-            pic, answer, is_success = Plotter.plot_levels(res, period)
-            Plotter.save_csv(res, period)
-            try:
-                bot.delete_message(
-                    message.chat.id,
-                    message.reply_to_message.message_id,
-                )
-            except AttributeError:
-                pass
-            finally:
-                bot.delete_message(message.chat.id, message.message_id)
-                bot.send_message(
-                    message.chat.id,
-                    answer,
-                    reply_markup=start_keyboard,
-                )
-                if is_success:
-                    bot.send_photo(message.chat.id, pic)
-                    with open('level.csv', 'rb') as csv:
-                        bot.send_document(message.chat.id, csv)
-            choice.clear()
-        except ValueError:
-            text = 'Ошибка в дате, попробуйте ещё раз в формате dd.mm.yyyy'
-            bot.delete_message(message.chat.id, message.message_id)
-            bot.send_message(message.chat.id, text, reply_markup=ForceReply())
-    else:
-        text = 'Сначала выберите водохранилище'
-        bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(message.chat.id, text)
+def manually_input_step(message):
+    try:
+        graph = graph_buffer[message.chat.id]
+        dates = re.findall(
+            r'[0-3][0-9][.][01][0-9][.][12][09][0-9][0-9]',
+            message.text,
+        )
+        date1 = dates[0]
+        date2 = None
+        if len(dates) > 1:
+            date2 = dates[-1]
+        period = TimePeriod(date1, date2, date_format='%d.%m.%Y')
+        graph.period = period
+        pic, text, is_success = plotting_graph(graph)
+        if is_success:
+            bot.delete_message(message.chat.id, message.message_id-1)
+            bot.send_photo(message.chat.id, pic, text)
+        else:
+            bot.delete_message(message.chat.id, message.message_id-1)
+            bot.reply_to(message, text)
+    except (ValueError, IndexError) as error:
+        logger.error(repr(error))
+        text = 'Ошибка в дате, попробуйте ещё раз в формате dd.mm.yyyy'
+        msg = bot.reply_to(message, text)
+        bot.register_next_step_handler(msg, manually_input_step)
 
 
-# Choice of reservoir
 @bot.message_handler(content_types=['text'])
 def list_reservoirs(message):
-    if message.text.lower() == 'показать список':
-        text = ('Выберите водохранилище')
+    if message.text.lower() == 'показать список водохранилищ':
+        text = 'Выберите водохранилище:'
         bot.delete_message(message.chat.id, message.message_id)
         bot.send_message(message.chat.id, text, reply_markup=res_keyboard)
 
 
-if __name__ == '__main__':
+def plotting_graph(graph):
+    reservoir = graph.reservoir
+    param = graph.param
+    period = graph.period
+    pic = None
+    text = 'Упс.. похоже я не знаю ответ.'
+    is_success = False
+    if param == 'levels':
+        pic, text, is_success = Plotter.plot_levels(reservoir, period)
+    elif param == 'inflow':
+        pic, text, is_success = Plotter.plot_inflow(reservoir, period)
+    elif param == 'outflow':
+        pic, text, is_success = Plotter.plot_outflow(reservoir, period)
+    return pic, text, is_success
+
+
+bot.polling()
+'''if __name__ == '__main__':
     while True:
         try:
             logging.info('Start polling')
             bot.polling()
         except requests.exceptions.ConnectionError as error:
             logging.error(repr(error))
-            time.sleep(30)
+            time.sleep(30)'''
