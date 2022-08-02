46476b285fb905f92dd64079a7c21aa27f935e58
Evgenii Khlystov <hlystovea@gmail.com>
2022-08-02 20:53:54 +0700

    Refactoring, added statistics endpoints

--- web/reservoirs/serializers.py
+++ web/reservoirs/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework.reverse import reverse
 from rest_framework.serializers import (FloatField, HyperlinkedIdentityField,
-                                        ModelSerializer, SerializerMethodField)
+                                        IntegerField, ModelSerializer,
+                                        Serializer, SerializerMethodField)
 
-from reservoirs.models import Reservoir, WaterSituation
 from common.serializers import RegionNestedSerializer
+from reservoirs.models import Reservoir, WaterSituation
 
 
 class ReservoirSerializer(ModelSerializer):
@@ -11,6 +12,8 @@ class ReservoirSerializer(ModelSerializer):
     region = RegionNestedSerializer()
     water_situations = SerializerMethodField()
     actual_situation = SerializerMethodField()
+    year_summary = SerializerMethodField()
+    statistics_by_doy = SerializerMethodField()
 
     class Meta:
         model = Reservoir
@@ -18,24 +21,30 @@ class ReservoirSerializer(ModelSerializer):
 
     def get_water_situations(self, obj):
         view_name = 'reservoirs:situation-list'
-        base_url = reverse(view_name, request=self.context['request'])
-        return f'{base_url}?reservoirs={obj.slug}'
+        return reverse(view_name, args=(obj.id, ), request=self.context['request'])  # noqa (E501)
 
     def get_actual_situation(self, obj):
         view_name = 'reservoirs:situation-actual'
-        base_url = reverse(view_name, request=self.context['request'])
-        return f'{base_url}?reservoirs={obj.slug}'
+        return reverse(view_name, args=(obj.id, ), request=self.context['request'])  # noqa (E501)
+
+    def get_year_summary(self, obj):
+        view_name = 'reservoirs:statistics-year-summary'
+        return reverse(view_name, args=(obj.id, ), request=self.context['request'])  # noqa (E501)
+
+    def get_statistics_by_doy(self, obj):
+        view_name = 'reservoirs:statistics-day-of-year'
+        return reverse(view_name, args=(obj.id, ), request=self.context['request'])  # noqa (E501)
 
 
-class WaterSituationSerializer(ModelSerializer):
+class SituationSerializer(ModelSerializer):
     avg_inflow = FloatField(read_only=True)
 
     class Meta:
         model = WaterSituation
-        fields = '__all__'
+        exclude = ['id', 'reservoir', 'free_capacity']
 
 
-class ActualWaterSituationSerializer(ModelSerializer):
+class ActualSituationSerializer(ModelSerializer):
     level_offset = SerializerMethodField(read_only=True)
     inflow_offset = SerializerMethodField(read_only=True)
     outflow_offset = SerializerMethodField(read_only=True)
@@ -62,3 +71,18 @@ class ActualWaterSituationSerializer(ModelSerializer):
         if obj.spillway_offset is None:
             return 'н/д'
         return f'{obj.spillway_offset:+d}'
+
+
+class YearSummarySerializer(Serializer):
+    year = IntegerField(read_only=True)
+    inflow_volume = FloatField(read_only=True)
+    outflow_volume = FloatField(read_only=True)
+    spillway_volume = FloatField(read_only=True)
+
+
+class StatisticsByDaySerializer(Serializer):
+    day_of_year = IntegerField(read_only=True)
+    max_inflow = IntegerField(read_only=True)
+    avg_inflow = IntegerField(read_only=True)
+    avg_outflow = IntegerField(read_only=True)
+    avg_spillway = IntegerField(read_only=True)
--- web/reservoirs/views.py
+++ web/reservoirs/views.py
@@ -1,28 +1,35 @@
-from django.db.models import F, Window
-from django.db.models.functions import Lag
+from django.db.models import Avg, F, Max, Sum, Window
+from django.db.models.functions import Extract, ExtractYear, Lag, Round
 from django.shortcuts import get_object_or_404
 from django.utils.timezone import now
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-from reservoirs.filters import ReservoirFilter
+from reservoirs.filters import ReservoirFilter, SituationFilter
 from reservoirs.models import Reservoir, WaterSituation
-from reservoirs.serializers import (ActualWaterSituationSerializer,
-                                    ReservoirSerializer,
-                                    WaterSituationSerializer)
-from reservoirs.utils import get_earlist_date, date_parse
+from reservoirs.serializers import (ActualSituationSerializer,
+                                    ReservoirSerializer, SituationSerializer,
+                                    StatisticsByDaySerializer,
+                                    YearSummarySerializer)
+from reservoirs.utils import date_parse, get_earlist_date
 
 
-class ReservoirsViewSet(ReadOnlyModelViewSet):
-    queryset = Reservoir.objects.select_related('region') \
-                        .order_by('region', 'id')
+class ReservoirViewSet(ReadOnlyModelViewSet):
     serializer_class = ReservoirSerializer
     filterset_class = ReservoirFilter
+    queryset = Reservoir.objects.select_related(
+        'region'
+    ).order_by(
+        'region',
+        'id'
+    )
 
 
-class WaterSituationsView(APIView):
+class WaterSituationView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request):
@@ -48,18 +55,47 @@ class WaterSituationsView(APIView):
 
         objs = WaterSituation.objects.raw(raw_query=raw_query, params=params)
 
-        serializer = WaterSituationSerializer(objs, many=True)
+        serializer = SituationSerializer(objs, many=True)
         return Response(serializer.data)
 
 
-class ActualWaterSituationsView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+class SituationViewSet(GenericViewSet):
+    queryset = WaterSituation.objects.order_by('date')
+    serializer_class = SituationSerializer
+    filterset_class = SituationFilter
 
-    def get(self, request):
-        reservoir_slug = request.query_params.get('reservoir', 'sayano')
-        reservoir = get_object_or_404(Reservoir, slug=reservoir_slug)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(reservoir=self.kwargs['reservoir_pk'])
 
-        situation = reservoir.situations.annotate(
+    def list(self, request, *args, **kwargs):
+        start = request.query_params.get('start', get_earlist_date())
+        end = request.query_params.get('end', now().date().isoformat())
+
+        reservoir = get_object_or_404(Reservoir, pk=kwargs['reservoir_pk'])
+
+        raw_query = '''
+            WITH ws AS (
+            SELECT *, avg(inflow) OVER w AS avg_inflow
+            FROM water_situation
+            WHERE reservoir_id = %s
+            WINDOW w AS (PARTITION BY DATE_PART('doy', date))
+            )
+            SELECT *
+            FROM ws
+            WHERE date BETWEEN %s AND %s
+            ORDER BY date
+        '''
+        params = [reservoir.id, start, end]
+
+        objs = WaterSituation.objects.raw(raw_query=raw_query, params=params)
+
+        serializer = SituationSerializer(objs, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['get'], url_name='actual', detail=False)
+    def actual(self, request, *args, **kwargs):
+        queryset = self.get_queryset().annotate(
             level_offset=F('level') - Window(
                 expression=Lag('level'),
                 order_by=F('date').asc(),
@@ -77,5 +113,49 @@ class ActualWaterSituationsView(APIView):
                 order_by=F('date').asc(),
             )
         ).latest('date')
-        serializer = ActualWaterSituationSerializer(situation)
+
+        serializer = ActualSituationSerializer(queryset)
+        return Response(serializer.data)
+
+
+class StatisticsViewSet(GenericViewSet):
+    queryset = WaterSituation.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(reservoir=self.kwargs['reservoir_pk'])
+
+    @action(methods=['get'], url_name='year-summary', detail=False)
+    def year_summary(self, request, *args, **kwargs):
+        queryset = self.get_queryset().annotate(
+            year=ExtractYear('date')
+        ).values(
+            'year'
+        ).annotate(
+            inflow_volume=Sum('inflow')*8.64e-5,
+            outflow_volume=Sum('outflow')*8.64e-5,
+            spillway_volume=Sum('spillway')*8.64e-5
+        ).order_by(
+            'year'
+        )
+
+        serializer = YearSummarySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['get'], url_name='day-of-year', detail=False)
+    def day_of_year(self, request, *args, **kwargs):
+        queryset = self.get_queryset().annotate(
+            day_of_year=Extract('date', 'doy')
+        ).values(
+            'day_of_year'
+        ).annotate(
+            max_inflow=Max('inflow'),
+            avg_inflow=Round(Avg('inflow')),
+            avg_outflow=Round(Avg('outflow')),
+            avg_spillway=Round(Avg('spillway'))
+        ).order_by(
+            'day_of_year'
+        )
+
+        serializer = StatisticsByDaySerializer(queryset, many=True)
         return Response(serializer.data)
