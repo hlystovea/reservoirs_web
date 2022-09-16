a83dbef946d916bd418abe24fa0f62ce3402ceb4
Evgenii Khlystov <hlystovea@gmail.com>
2022-09-16 22:32:17 +0700

    The max-age of cookie has been changed

--- web/assets/js/utils.js
+++ web/assets/js/utils.js
@@ -1,6 +1,6 @@
 // sets cookie
 function setCookie(name, value) {
-  document.cookie = name + "=" + (value || "") + "; path=/; max-age=86400";
+  document.cookie = name + "=" + (value || "") + "; path=/; max-age=" + 100*24*60*60*1000;
 }
 
 // gets cookie
