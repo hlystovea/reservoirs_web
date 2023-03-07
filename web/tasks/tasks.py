9da04d6714d2c126386d6c0963c299ce6493ee56
Evgenii Khlystov <hlystovea@gmail.com>
2023-03-07 09:36:29 +0700

    Refactoring parser classes

--- web/tasks/tasks.py
+++ web/tasks/tasks.py
@@ -2,18 +2,18 @@ from celery.utils.log import get_task_logger
 from httpx import HTTPError
 
 from web.celery import app
-from services.parsers import KrasParser, RushydroParser
+from services.scrapers import KrasScraper, RushydroScraper
 
 logger = get_task_logger(__name__)
 
 
 @app.task(autoretry_for=(HTTPError, ), retry_backoff=20)
 def run_rushydro_parsing():
-    RushydroParser().worker()
+    RushydroScraper.scrape()
     return True
 
 
 @app.task(autoretry_for=(HTTPError, ), retry_backoff=20)
 def run_kras_parsing():
-    KrasParser().worker()
+    KrasScraper.scrape()
     return True
