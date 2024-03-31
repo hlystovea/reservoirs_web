3c571a90733a4e74ca01864e0da448015edcec5f
Evgenii Khlystov <hlystovea@gmail.com>
2024-03-31 17:28:30 +0700

    Ref: combine the same functions

--- web/predictors/views.py
+++ web/predictors/views.py
@@ -1,7 +1,7 @@
 from django.db.models import OuterRef, Subquery
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from predictors.filters import ForecastFilter
+from predictors.filters import ForecastFilter, PredictorFilter
 from predictors.models import WaterSituationForecast, WaterSituationPredictor
 from predictors.serializers import PredictorSerializer, ForecastSerializer
 from reservoirs.models import WaterSituation
@@ -10,7 +10,7 @@ from reservoirs.models import WaterSituation
 class PredictorsViewSet(ReadOnlyModelViewSet):
     serializer_class = PredictorSerializer
     queryset = WaterSituationPredictor.objects.all()
-    filterset_fields = ('reservoir', )
+    filterset_class = PredictorFilter
 
 
 class ForecastViewSet(ReadOnlyModelViewSet):
